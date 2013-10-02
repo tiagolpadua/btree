fc509e1dcf840f1d99f42b36c9f3da9ffc99a756
Tiago Padua <tiagolpadua@gmail.com>
2013-10-02 19:57:24 -0300

    Commit inicial

---
+++ src/btree.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : btree.c
+ Author      : Tiago Lage Payne de Pádua, Pedro Mohn
+ Version     : 0.1
+ Copyright   : UnB
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World"); /* prints Hello World */
+	return EXIT_SUCCESS;
+}
